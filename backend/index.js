2285d4e17b385fa28699ef3658c425ea8f3dff88
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-11-15 13:28:53 -0800

    generation class
    generation class is parent of dragon class.

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,37 @@
-const Dragon = require(`./dragon.js`)
+// const Dragon = require(`./dragon.js`)
 
-const foo = new Dragon({ 
-    bday: "Luis bday", 
-    nickName: "Luis Name" 
-});
+// const foo = new Dragon({ 
+//     bday: "Luis bday", 
+//     nickName: "Luis Name" 
+// });
 
-const zeek = new Dragon({ 
-    bday: new Date(), 
-    nickName: "Zeek Name",
-    traits: [{type: "backgroundColor", traitValue: "green"}]
-});
+// const zeek = new Dragon({ 
+//     bday: new Date(), 
+//     nickName: "ZEEK",
+//     traits: [{type: "backgroundColor", traitValue: "black"}]
+// });
 
-const prince = new Dragon();
+// // const prince = new Dragon();
+
+// setTimeout(() => {
+//     const odin = new Dragon();
+//     console.log(`odin`, odin);
+// }, 3000);
+
+// // console.log(`foo`, foo);
+// console.log(`zeek`, zeek);
+// console.log();
+// // console.log(`prince`, prince);
+
+const Geneartion = require('./generation');
+
+const generation = new Geneartion;
+console.log('generation:\n', generation);
+
+const odin = generation.newDragon();
+console.log("new drag Odin: ", odin);
 
 setTimeout(() => {
-    const odin = new Dragon();
-    console.log(`odin`, odin);
-}, 3000);
-
-console.log(`foo`, foo);
-console.log(`zeek`, zeek);
-console.log(`prince`, prince);
+    const maria = generation.newDragon();
+    console.log('maria:', maria);
+}, 5000);
