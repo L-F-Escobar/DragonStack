2e386a30eba5fc88036ddd288b9feb1a32db028f
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-11-15 19:07:37 -0800

    api working

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,33 +1,17 @@
-// const Geneartion = require('./generation');
-
-// const generation = new Geneartion;
-// console.log('generation:\n', generation, '\n');
-
-// const odin = generation.newDragon();
-// console.log("new drag Odin: ", odin, '\n');
-
-// setTimeout(() => {
-//     const maria = generation.newDragon();
-//     console.log('maria:', maria, '\n');
-// }, 10000);
-
-
 // express is a represantation of a web server application full of helper methods
 const express = require('express');
-const GenerationEgine = require('./generation/engine.js');
+const GenerationEngine = require('./generation/engine.js');
+const dragonRouter = require('./api/dragon.js');
+const generationRouter = require('./api/generation.js')
 
 const app = express();
-const engine = new GenerationEgine();
+const engine = new GenerationEngine();
+
+app.locals.engine = engine;
+
+app.use('/dragon', dragonRouter);
+app.use('/generation', generationRouter);
 
 engine.start();
-
-// (url, callback func)
-// req = request 
-// res = response
-app.get('/dragon/new', (req, res) => {
-    res.json({ dragon: engine.generation.newDragon() });
-});
-
-
 
 module.exports = app;
