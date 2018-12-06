1c794e32d69af197dbdb926f5b4c0971fa64a69c
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-12-05 16:16:20 -0800

    can access localhost:5000 from frontend localhost:1234

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 
 // By extending, this class gets preloaded with a much of methods & properties.
 class Generation extends Component {
+    state = { generation: { generationId: 999, expiration: "2020-06-30" } };
+    // constructor() {
+    //     this.state = {};
+    // } //same thing
+
+    componentDidMount() {
+        this.fetchGeneration();
+    }
+    
+    fetchGeneration = () => {
+        fetch("http://localhost:5000/generation")
+            .then( response => console.log('response6666', response));
+    }
+    
     render() {
-        const generation = { "generationId": 999, expiration: "2020-06-30"};
+        // generation is an object
+        const { generation } = this.state;
+        // const generation = this.state.generation; //same thing
+
         return (
             <div>
                 <h3>Generation {generation.generationId}. Expires on:</h3>
