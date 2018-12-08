1c0976d789ee9a92e330aebba6d4b819b386aab5
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-12-07 23:50:01 -0800

    stuck on the forEach traits problem. awaiting response

--- frontend/src/components/DragonAvatar.js
+++ frontend/src/components/DragonAvatar.js
@@ -14,9 +14,38 @@ const propertyMap = {
 };
 
 class DragonAvatar extends Component {
+
+    get DragonImage() {
+
+        const dragonPropertyMap = {};
+        console.log('DRAGON', this.props.dragon);
+        console.log('TRAITS', this.props.dragon.traits);
+
+        this.props.dragon.traits.forEach(trait => {
+            console.log('TRAIT666', trait);
+            const { traitType, traitValue } = trait;
+
+            dragonPropertyMap[traitType] = propertyMap[traitType][traitValue];
+        });
+
+        const { backgroundColor, build, pattern, size } = dragonPropertyMap;
+
+        const sizing = { width: size, height: size };
+
+        return (
+            <div className='dragon-avatar-image-wrapper'>
+                <div className='dragon-avatar-image-background' style={{ backgroundColor, ...sizing}}></div>
+                    <img src={pattern} className='dragon-avatar-image-pattern' style={{ ...sizing }}/>
+                    <img src={build} className='dragon-avatar-image' style={{ ...sizing }}/>
+            </div>
+        );
+    }
+
     render() {
         // props = properties inheritance from parent to child a data object
         const { dragon } = this.props.dragon;
+
+        if (!dragon.dragonId) return <div></div>;
 
         return (
             <div>
@@ -24,6 +53,7 @@ class DragonAvatar extends Component {
                 <span>Dragon Id: {dragon.dragonId}. </span>
 
                 {dragon.traits.map(trait => trait.traitValue).join(', ')}
+                { this.DragonImage }
             </div>
         )
     }
